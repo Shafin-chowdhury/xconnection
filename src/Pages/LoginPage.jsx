4a3efe187fbd52e7b1c77e75a8dab22dae889d1c
Shafin Chowdhury <shafinchowdhury2001@gmail.com>
2025-10-17 23:15:25 +0600

    Fix: corrected import path case

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import AuthIllustration from "/images/auth_illustration.png"
 import { Link } from 'react-router-dom'
-import LoginForm from '../Components/Auth/loginForm'
+import LoginForm from '../Components/Auth/LoginForm'
 
 const LoginPage = () => {
   return (
